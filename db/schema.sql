d2763752466dbb5d40bb6a4f71e6fb16e9a7c1f0
Huiling Wu <huwu@ucdavis.edu>
2019-01-16 14:55:10 -0800

    created tables in mysql

---
+++ db/schema.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS todo_db;
+
+CREATE DATABASE todo_db;
+
+USE todo_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255),
+    creator_id INT NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES users(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users_projects (
+    user_id INT NOT NULL,
+    project_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (project_id) REFERENCES projects(id)
+);
+
+CREATE TABLE todos (
+    id INT AUTO_INCREMENT NOT NULL,
+    task VARCHAR(500),
+    project_id INT NOT NULL,
+    belongs_to INT,
+    finished BOOLEAN,
+    FOREIGN KEY (project_id) REFERENCES projects(id),
+    FOREIGN KEY (belongs_to) REFERENCES users(id),
+    PRIMARY KEY (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+USE todo_db;
+
